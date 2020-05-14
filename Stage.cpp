a6004880ed4588876955d08bc4f8b0ac3f140384
yutoto <yuto.w4613@ezweb.ne.jp>
2020-05-14 11:53:30 +0900

    ステージの追加

---
+++ Stage.cpp
@@ -0,0 +1,31 @@
+#include "DxLib.h"
+#include "main.h"
+#include "Stage.h"
+
+int mapImage[40];
+int map[MAP_Y][MAP_X];
+
+// ínê}ÇÃï`âÊ
+int satage[MAP_Y][MAP_X] =
+{
+	{1,1,1,1,1},
+	{1,1,1,1,1},
+	{1,1,1,1,1},
+	{1,1,1,1,1},
+	{1,1,1,1,1}
+};
+
+void StageSystemInit(void)
+{
+	//
+	LoadDivGraph("image/map.bmp", 40, 10, 4, CHIP_SIZE_X, CHIP_SIZE_Y, mapImage);
+}
+void StageGameDraw(void)
+{
+	DrawGraph(CHIP_SIZE_X, CHIP_SIZE_Y, mapImage[40], true);
+}
+
+void SetMapData()
+{
+
+}
---
+++ Stage.h
@@ -0,0 +1,11 @@
+//#pragma once
+
+#define MAP_X 5			// 縦方向の数
+#define MAP_Y 5			// 横方向の数
+#define CHIP_SIZE_X 32		// チップ1つのサイズX
+#define CHIP_SIZE_Y 32		// チップ1つのサイズY
+
+
+void StageGameDraw(void);
+void StageSystemInit(void);
+void SetMapData();
